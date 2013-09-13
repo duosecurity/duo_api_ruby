49429512a297e479a1143f4ae925e405b4eeaede
Ben Murray <ben@duosecurity.com>
2013-09-13 15:38:49 -0400

    Initial import

---
+++ duo_api.rb
@@ -0,0 +1,62 @@
+require 'digest/hmac'
+require 'net/https'
+require 'time'
+require 'uri'
+
+class DuoApi
+  @@encode_regex = Regexp.new("[^-_.~a-zA-Z\\d]")
+
+  def initialize(ikey, skey, host)
+    @ikey = ikey
+    @skey = skey
+    @host = host
+  end
+
+  def request(method, path, params=nil)
+    uri = request_uri(path, params)
+    current_date, signed = sign(method, uri.host, path, params, @skey)
+
+    request = Net::HTTP.const_get(method.capitalize).new uri.to_s
+    request.basic_auth(@ikey, signed)
+    request['Date'] = current_date
+
+    response = Net::HTTP.start(uri.host, uri.port, :use_ssl => true) do |http|
+      http.request(request)
+    end
+  end
+
+  private
+  def encode_params(params_hash=nil)
+    return "" if params_hash.nil?
+    params_hash.sort.map do |k,v|
+      URI::encode(k.to_s, @@encode_regex) + "=" + URI::encode(v.to_s, @@encode_regex)
+    end.join("&")
+  end
+
+  def get_time
+    Time.now.rfc2822
+  end
+
+  def request_uri(path, params=nil)
+    u = 'https://' + @host + path
+    u += '?' + encode_params(params) unless params.nil?
+    URI.parse(u)
+  end
+
+  def canonicalize(method, host, path, params, options={})
+    options[:date] ||= get_time
+    canon = [
+      options[:date],
+      method.upcase,
+      host.downcase,
+      path,
+      encode_params(params)
+    ]
+    [options[:date], canon.join("\n")]
+  end
+
+  def sign(method, host, path, params, skey, options={})
+    date, canon = canonicalize(method, host, path, params, :date => options[:date])
+    [date, Digest::HMAC.hexdigest(canon, @skey, Digest::SHA1)]
+  end
+end
---
+++ examples.rb
@@ -0,0 +1,30 @@
+require_relative 'duo_api'
+
+IKEY = ''
+SKEY = ''
+HOST = ''
+
+# Initialize the api
+client = DuoApi.new IKEY, SKEY, HOST
+
+# EXAMPLE 1: Get all users
+resp = client.request 'GET', '/admin/v1/users'
+
+# print out some info from the response
+puts resp.code
+puts resp.header.to_hash
+puts resp.message
+puts resp.http_version
+puts resp.body
+
+# EXAMPLE 2: retreive the user 'john'
+resp2 = client.request 'GET', '/admin/v1/users', {username: 'john'}
+puts resp2.body
+
+# EXAMPLE 3: create a new user
+resp3 = client.request 'POST', '/admin/v1/users', {username: 'john2'}
+puts resp3.body
+
+# EXAMPLE 4: delete user with user_id: 'DUAE0W526W52YHOBMDO6'
+resp4 = client.request 'DELETE', '/admin/v1/users/DUAE0W526W52YHOBMDO6'
+puts resp4.body
