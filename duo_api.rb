a53e48c12523e349bc7448f556e59ed63e35c0b8
Richo Healey <richo@psych0tik.net>
2014-05-10 13:37:12 -0700

    force verify_mode

--- duo_api.rb
+++ duo_api.rb
@@ -23,7 +23,8 @@ class DuoApi
     ca_file = File.join(File.dirname(__FILE__), "cacert.pem")
 
     response = Net::HTTP.start(uri.host, uri.port,
-                               :use_ssl => true, :ca_file => ca_file) do |http|
+                               :use_ssl => true, :ca_file => ca_file,
+                               :verify_mode => OpenSSL::SSL::VERIFY_PEER) do |http|
       http.request(request)
     end
   end
